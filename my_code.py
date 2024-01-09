f5100a0c5a6c3c328888f9b414eb1cfedd9eea40
Jeann Espinal <jeannespinal985@mail.com>
2024-01-09 10:24:33 -0500

    first commit

---
+++ my_code.py
@@ -0,0 +1,11 @@
+def main() :
+    name = "Jeann Espinal"
+    print("We want to know if you love programming!")
+    print()
+    print("Do you like programming " + name + "?")
+    answer = input()
+    print("Great! you said " + answer + "?")
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()
